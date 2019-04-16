8087dc5f89466509858092722da70241b1e8b00e
Brodie Brock <brodiebrock@Brodies-MacBook-Air.local>
2019-04-16 15:26:47 -0400

    I added some new notes

---
+++ firstProject.py/firstProject.py
@@ -0,0 +1,5 @@
+FIRSTNAME = input('What is your FIRSTNAME? ')
+LASTNAME = input('what is your LASTNAME? ')
+COLOR = input('What is your favorite COLOR? ') 
+
+print("Hello %s %s ! my favorite color is %s " %  (FIRSTNAME, LASTNAME, COLOR))
